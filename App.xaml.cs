51e53a88631e0bead2197e11aa1cb5f2b949d6aa
Raul Luis <soyrauluis@outlook.com>
2022-01-30 18:10:53 -0400

    Add exception handling

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
+using System.Diagnostics;
+
 
 namespace Tarea2
 {
@@ -15,8 +17,47 @@ namespace Tarea2
     public partial class App : Application
     {
 
-        public App()
+        public App() : base()
         {
+            SetupUnhandledExceptionHandling();
+        }
+
+        private void SetupUnhandledExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+                ShowUnhandledException(args.ExceptionObject as Exception, "AppDomain.CurrentDomain.UnhandledException", false);
+
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+                ShowUnhandledException(args.Exception, "TaskScheduler.UnobservedTaskException", false);
+
+            Dispatcher.UnhandledException += (sender, args) =>
+            {
+                if (!Debugger.IsAttached)
+                {
+                    args.Handled = true;
+                    ShowUnhandledException(args.Exception, "Dispatcher.UnhandledException", true);
+                }
+            };
+
+        }
+
+        void ShowUnhandledException(Exception e, string unhandledExceptionType, bool promptUserForShutdown)
+        {
+            var messageBoxTitle = $"Unexpected Error Occurred: {unhandledExceptionType}";
+            var messageBoxMessage = $"The following exception occurred:\n\n{e}";
+            var messageBoxButtons = MessageBoxButton.OK;
+
+            if (promptUserForShutdown)
+            {
+                messageBoxMessage += "\n\nNormally the app would die now. Should we let it die?";
+                messageBoxButtons = MessageBoxButton.YesNo;
+            }
+
+            if (MessageBox.Show(messageBoxMessage, messageBoxTitle, messageBoxButtons) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+
         }
     }
 }
